0896445c4e0d0d340ddd841d5b93ed5a0b56805a
umidjanovnaa <sheraliyevaruxshona08@gmail.com>
2024-02-23 23:18:30 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+# # num = [1,2,2,3,3,5,6,6,9]
+# nums = []
+# for x in num:
+#     if x not in nums:
+#         nums.append(x)
+# print(nums)
+
+# for x in num:
+#     if x == 9:
+#      print("bor")
+
+
+# 1-nasala
+# family = {
+#     "father":"Darth Vader",
+#     "sister": "Leia",
+#     "brother in law": "Han",
+#     "droid": "R2D2",
+# }
+# name = input("Name: ")
+# def relation_to_luke(name):
+#         for x in family:
+#             if family[x] == name:
+#                 return f"Luke, I am your {x}."
+#             elif family[x] == name:
+#                 return f"Luke, I am your {x}."
+#
+#             elif family[x] == name:
+#                 return f"Luke, I am your {x}."
+# print(relation_to_luke(name))
+
+# 2-masala
+# def get_student_names(lst):
+#     sort_name = []
+#     for x in lst:
+#         sort_name.append(lst[x])
+#     sort_name.sort()
+#     return sort_name
+# print(get_student_names({
+#   "Student 1" : "Steve",
+#   "Student 2" : "Becky",
+#   "Student 3" : "John"
+# }))
+
+# 3-masala
+# def cancul(circy, circy1):
+#
+#     return circy1
+# print(cancul([11], []))
+# print(cancul([4.44], []))
+
+# 4-masala
+# def maximum_score(lst):
+#     rezalt = 0
+#     for x in lst:
+#         rezalt += x.get("score")
+#     return rezalt
+#
+# print(maximum_score([
+#   {"tile": "N", "score": 1},
+#   {"tile": "K", "score": 5},
+#   {"tile": "Z", "score": 10},
+#   {"tile": "X", "score": 8},
+#   {"tile": "D", "score": 2},
+#   {"tile": "A", "score": 1},
+#   {"tile": "E", "score": 1}
+# ]))
+
+# 5-masala
